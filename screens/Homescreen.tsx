0779e6336d94da24d60fc5438355de8dfd89fdf4
MAV3R1CK <eddymav247@gmail.com>
2022-10-31 10:34:33 +0100

    search input

--- screens/Homescreen.tsx
+++ screens/Homescreen.tsx
@@ -4,6 +4,7 @@ import {
   ActivityIndicator,
   FlatList,
   Pressable,
+  TextInput,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import Book from "../utils/utils";
@@ -11,16 +12,20 @@ import BookCard from "../components/BookCard";
 import { useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../App";
 import { StackNavigationProp } from "@react-navigation/stack";
-import SearchInput from "../components/SearchInput";
 
 type detailsScreenProps = StackNavigationProp<RootStackParamList, "Details">;
 
 const Homescreen = () => {
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState();
-  const [data, setData] = useState<Book | undefined>();
+  const [data, setData] = useState<Book[] | undefined[]>([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const navigation = useNavigation<detailsScreenProps>();
+
+  // filter((searchTerm) => data[index].title === searchTerm);
+  // }
 
   useEffect(() => {
     fetch("https://fudap-books-api.herokuapp.com/books/")
@@ -29,13 +34,33 @@ const Homescreen = () => {
         (result) => {
           setLoading(false);
           setData(result);
+          setFilteredData(result);
         },
         (error) => {
           setLoading(false);
           setError(error);
         }
       );
-  });
+  }, []);
+
+  const searchFilterFunction = (text) => {
+    if (text) {
+      const newData = data.filter(function (item) {
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : "".toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+      setSearchTerm(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredData(data);
+      setSearchTerm(text);
+    }
+  };
 
   const getContent = () => {
     if (isLoading) {
@@ -54,8 +79,8 @@ const Homescreen = () => {
     }
     return (
       <FlatList
-        data={data}
-        keyExtractor={(books, index) => {
+        data={filteredData}
+        keyExtractor={(books) => {
           return books.id;
         }}
         renderItem={({ item }) => {
@@ -78,7 +103,7 @@ const Homescreen = () => {
             >
               <BookCard books={item} accessibilityLabel="books" />
             </Pressable>
-          );ƒ
+          );
         }}
       />
     );
@@ -86,7 +111,13 @@ const Homescreen = () => {
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      <SearchInput />
+      <View className="p-2 mx-1 mt-[20px] bg-gray-300 rounded">
+        <TextInput
+          placeholder="Search For books"
+          onChangeText={(text) => searchFilterFunction(text)}
+          value={searchTerm}
+        />
+      </View>
       <View className="mt-8">{getContent()}</View>
     </View>
   );
